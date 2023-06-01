d75cd706abb171ece0dcdae918be62de29a72971
🦄Pegasus (DarkStar☀) <darkstar@unisocial.net>
2023-06-01 17:56:54 -0500

    Armbian build

--- src/Armbian build/ansible/roles/build/files/test-build.sh
+++ src/Armbian build/ansible/roles/build/files/test-build.sh
@@ -1,3 +1,9 @@
 # Test Build
 
-./compile.sh KERNEL_GIT=shallow
+simplest(){
+    ./compile.sh KERNEL_GIT=shallow
+}
+
+desktop(){
+    ./compile.sh build BOARD=rk3318-box BRANCH=edge BUILD_DESKTOP=yes BUILD_MINIMAL=no DESKTOP_APPGROUPS_SELECTED=3dsupport DESKTOP_ENVIRONMENT=gnome DESKTOP_ENVIRONMENT_CONFIG_NAME=config_base KERNEL_CONFIGURE=no KERNEL_GIT=shallow RELEASE=bookwor
+}
