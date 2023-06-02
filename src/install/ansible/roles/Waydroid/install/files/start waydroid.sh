2ea5c4a94c2e9a34dbc6c8c4a2afa642258b4bc7
🦄Pegasus (DarkStar☀) <darkstar@unisocial.net>
2023-06-02 12:27:03 -0500

    try all

--- src/install/ansible/roles/Waydroid/install/files/start waydroid.sh
+++
@@ -1,7 +0,0 @@
-sudo waydroid container start
-# OR
-sudo systemctl enable waydroid-container.service
-sudo systemctl start waydroid-container.service
-
-waydroid session start &
-waydroid show-full-ui
