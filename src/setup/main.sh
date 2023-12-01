49397397dd8d444546c276fb21f781bd5d79b031
DarkStar☀ <61005029+Twin-Flame@users.noreply.github.com>
2023-11-30 20:28:26 -0600

    Update main.sh

--- src/setup/main.sh
+++ src/setup/main.sh
@@ -8,6 +8,11 @@ sudo venv/bin/python3 main.py certified
 
 sudo venv/bin/python3 main.py install smartdock
 
+session_start(){
+  waydroid session start
+}
+
 multi_window_mode(){
+  session_start()
   waydroid prop set persist.waydroid.multi_windows true
 }
